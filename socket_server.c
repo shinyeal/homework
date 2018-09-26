62724d80ef0d97d5da25c76c3667d7b748c8bd6c
shin_yeal <1670806387@qq.com>
2018-09-26 16:01:16 +0800

    socket服务端

--- socket_server.c
+++ socket_server.c
@@ -1,150 +1,81 @@
 /*************************************************************************
-	> File Name: socket_server.c
-	> Author: 
-	> Mail: 
-	> Created Time: 2018年09月24日 星期一 11时29分59秒
- ************************************************************************/
+> File Name: socket_server.c
+> Author: 
+> Mail: 
+> Created Time: 2018年09月25日 星期二 19时13分13秒
+************************************************************************/
 
-#include <arpa/inet.h>
-#include <ctype.h>
-#include <dirent.h>
-#include <errno.h>
-#include <fcntl.h>
-#include <netdb.h>
-#include <netinet/in.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/wait.h>
-#include <sys/file.h>
-#include <signal.h>
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <unistd.h>
-#include <time.h>
-#include <stdarg.h>
+#include<stdio.h> 
+#include <stdlib.h> 
+#include <string.h> 
+#include <errno.h> 
+#include <sys/socket.h> 
+#include <arpa/inet.h> 
+#include <netinet/in.h> 
+#include <sys/types.h> 
+#include <unistd.h> 
+#include <sys/time.h> 
 
-#define PORT 1028  //端口号
-#define LISTNUM 100   //最大监听数
-#define BUFFLEN 1024 //消息最大字节数
-
+#define BUFLEN 1000
+#define PORT 1028
+#define LISTNUM 20
+//#define host "192.168.1.156"
 int main() {
+    int a = 0;
+    int pid;
+    int sockfd, newfd; 
+    struct sockaddr_in s_addr, c_addr;  
+    socklen_t len; 
+    int port, listnum; 
     
-    int sockfd, newfd;
-    struct sockaddr_in s_addr, c_addr;
-    char buf[BUFFLEN];
-    socklen_t len;
-    unsigned int port, listnum;
-    fd_set rfds;  //文字个数
-    //struct timeavl tv;
-    int retval;
-    int maxfd;
+    /*建立socket*/ 
+    if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) { 
+        perror("socket"); 
+        return -1;
+    } 
 
-    //建立socket
-    if((sockfd = socket(PF_INET, SOCK_STREAM, 0)) == -1) {
-        perror("socket");
-        exit(errno);
-    } else {
-        printf("socket create success!\n");
+    s_addr.sin_family = AF_INET; 
+    s_addr.sin_port = htons(PORT); 
+    s_addr.sin_addr.s_addr = htons(INADDR_ANY); 
+    //s_addr.sin_addr.s_addr = inet_addr(host);
+    
+    /*把地址和端口帮定到套接字上*/ 
+    if((bind(sockfd, (struct sockaddr*) &s_addr,sizeof(struct sockaddr))) < 0){ 
+        perror("bind"); 
+        return -1;
     }
 
-    memset(&s_addr, 0, sizeof(s_addr));
-    s_addr.sin_family = AF_INET;
-    s_addr.sin_port = htons(PORT);
-    s_addr.sin_addr.s_addr = htons(INADDR_ANY);
-
-    //把地址和端口绑定到套接字上
-    if((bind(sockfd, (struct sockaddr*) &s_addr, sizeof(struct sockaddr))) == -1) {
-        perror("bind");
-        exit(errno);
-    } else {
-        printf("bind success!\n");
+    /*侦听本地端口*/ 
+    if(listen(sockfd,listnum) < 0){ 
+        perror("listen"); 
+        exit(1);
     }
 
-    //监听
-    if(listen(sockfd, listnum) == -1) {
-        perror("listen");
-        exit(errno);
-    } else {
-        printf("listening now!\n");
-    }
-
-    while(1) {
-        printf("----chat list----\n");
-        len = sizeof(struct sockaddr);
-        if((newfd = accept(sockfd,(struct sockaddr*) &c_addr, &len)) == -1) {
-            perror("accept");
+    while(1){ 
+        printf("*****************聊天开始***************\n"); 
+        len = sizeof(struct sockaddr); 
+        if((newfd = accept(sockfd,(struct sockaddr*) &c_addr, &len)) < 0) { 
+            perror("accept"); 
             exit(errno);
-        } else {
-            printf("已连接IP地址 ：%s : %d\n", inet_ntoa(c_addr.sin_addr), ntohs(c_addr.sin_port));
-        }
+        }else 
+        printf("正在与您聊天的客户端是：%s: %d\n",inet_ntoa(c_addr.sin_addr), ntohs(c_addr.sin_port));
 
-        while(1) {
-            FD_ZERO(&rfds);
-            FD_SET(0, &rfds);
-            maxfd = 0;
-            FD_SET(newfd, &rfds);
-            
-            if(maxfd < newfd) {
-                maxfd = newfd;
-            }
-
-            retval = select(maxfd + 1, &rfds, NULL, NULL, 0);
-            if(retval == -1) {
-                perror("select");
-                exit(errno);
-            } else {
-
-            //用户发送信息
-            if(FD_ISSET(0, &rfds)) {
-                memset(buf, 0, sizeof(buf));
-                //fget函数: 从流中读取BUFFLEN-1个字符
-                fgets(buf, BUFFLEN, stdin);
-                //打印消息
-                fputs(buf, stdout);
-
-                if(!strncasecmp(buf, "quit", 1)) {
-                    printf("server 请求终止聊天\n");
-                    break;
+            if((pid = fork()) < 0)
+                printf("Error forking child process");
+            if(pid == 0) {
+                char buffer[BUFLEN];
+                while((a = recv(newfd, buffer, BUFLEN, 0)) > 0) {
+                    buffer[a] = '\0';   
+                    printf("recv %d 字节 %s\n", a, buffer);
+                    fflush(stdout);
+                    memset(buffer, 0, sizeof(buffer));
                 }
-                    len = send(newfd, buf, strlen(buf), 0);
-                if(len > 0) {
-                    printf("\t发送成功 ：%s\n", buf);
-                } else {
-                    printf("发送失败\n");
-                    break;
-                }
+                printf("\n");
+                close(newfd);
+                exit(0);
             }
-
-            //客户端发来消息
-            if(FD_ISSET(newfd, &rfds)) {
-                memset(buf, 0, sizeof(buf));
-                len = recv(newfd, buf, BUFFLEN, 0);
-                if(len > 0) {
-                    printf("客户端发来的消息 ： %s\n", buf);
-                } else if(len < 0) {
-                    printf("接收失败\n");
-                } else {
-                    printf("客户端退出，结束聊天\n");
-                }
-                break;
-            }
-
-        }
-}
-
-        //关闭聊天套接字
-        close(newfd);
-        printf("是否退出(y/n)");
-        bzero(buf, BUFFLEN);
-        fgets(buf, BUFFLEN, stdin);
-        if(!strncasecmp(buf, "y", 1)) {
-            printf("server 退出\n");
-            break;
-        }
     }
 
-    close(sockfd);
+        close(sockfd);
     return 0;
 }
-
